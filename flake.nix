0ad75cf80f0fcbad4c1a3697db2a33f0e4183147
Linus Heckemann <git@sphalerite.org>
2023-11-06 18:29:29 +0100

    Setup that can successfully load shim
    Shim then fails to load GRUB due to missing SBAT.

--- flake.nix
+++ flake.nix
@@ -7,6 +7,8 @@
     flake-parts.url = "github:hercules-ci/flake-parts";
     nixpkgs.url = "github:lheckemann/nixpkgs/shim";
     lanzaboote.url = "github:nix-community/lanzaboote/v0.3.0";
+    # Work around OVMF with broken secure boot
+    nixpkgs-old.url = github:nixos/nixpkgs/8f40f2f90b9c9032d1b824442cfbbe0dbabd0dbd;
   };
   outputs = inputs: inputs.flake-parts.lib.mkFlake { inherit inputs; } (
     toplevel@{ config, self, ... }: {
@@ -14,10 +16,10 @@
     flake.hydraJobs = {
       inherit (self) packages;
     };
-    perSystem = { config, pkgs, self', ... }: {
+    perSystem = { config, pkgs, self', system, ... }: {
       packages = {
         shim-unsigned = pkgs.shim-unsigned.override {
-          vendorCertFile = ./pki/snakeoil-vendor-cert.pem;
+          vendorCertFile = ./pki/snakeoil-vendor-cert.cer;
         };
         # TODO: put it in passthru for shimx64.efi
         shim-signed = pkgs.runCommand "sign-shim" {} ''
@@ -53,37 +55,68 @@
           set -exuo pipefail
           args=(
             -m 2G -smp 4
-            -bios ${pkgs.OVMF.fd}/FV/OVMF.fd
+            -bios ${self'.packages.ovmf}/FV/OVMF.fd
             -drive if=virtio,file=${self'.packages.test-image}/test.qcow2,snapshot=on
             -net none
-            #-cdrom ~/deploy/result/iso/nixos.iso
+            -cdrom ${self'.packages.iso}/iso/nixos.iso
           )
           ${pkgs.qemu_kvm}/bin/qemu-kvm "''${args[@]}"
         '';
 
         # nix build .#iso
-        # qemu-kvm -m 2G -smp 4 -bios $ovmf/FV/OVMF.fd -cdrom result/iso/*.iso -net none
+        # qemu-kvm -m 2G -smp 4 -cdrom result/iso/*.iso -net none
+        # -drive "if=pflash,format=raw,readonly=on,file=$ovmf/FV/OVMF_CODE.fd" -drive "if=pflash,format=raw,snapshot=on,file=$ovmf/FV/OVMF_VARS.fd"
         iso = let
-          configModule = { modulesPath, ... }: {
+          configModule = { modulesPath, pkgs, ... }: {
             imports = [
               #(modulesPath + "/installer/cd-dvd/installation-cd-minimal.nix")
               (modulesPath + "/installer/cd-dvd/iso-image.nix")
             ];
             secureboot = {
               signingCertificate = ./pki/snakeoil-vendor-cert.pem;
+              # TODO: use hardware module or something
               privateKeyFile = ./pki/snakeoil-vendor-key.pem;
               shim = "${self'.packages.shim-signed}/share/shim/shimx64.efi";
             };
             # for faster build
             isoImage.squashfsCompression = "zstd -Xcompression-level 6";
             isoImage.makeEfiBootable = true;
+
+            users.users.root.password = "";
+            environment.systemPackages = [
+              pkgs.sbctl
+              pkgs.sbsigntool
+              pkgs.efivar
+              pkgs.vim
+              (pkgs.writeShellScriptBin "enroll-keys" ''
+                set -exuo pipefail
+                # the only key that actually matters is the db one, so generate the others
+                sbctl create-keys
+                # and then replace the generated db cert with our test one
+                sbctl import-keys --db-cert ${./pki/snakeoil-db-uefi.pem} --db-key ${./pki/snakeoil-db-uefi.key}
+                # yolo (this shouldn't be done outside test environments!)
+                sbctl enroll-keys --yes-this-might-brick-my-machine
+                set-shim-verbose
+              '')
+              (pkgs.writeShellScriptBin "set-shim-verbose" ''
+                cd /sys/firmware/efi/efivars
+                printf "\x01" >verbose
+                efivar --name 605dab50-e046-4300-abb6-3dd810dd8b23-SHIM_VERBOSE -w -f verbose
+              '')
+            ];
+            systemd.tmpfiles.rules = [
+              "L+ /run/sbkeys - - - - ${./pki}"
+            ];
           };
           nixos = pkgs.nixos configModule;
         in nixos.config.system.build.isoImage;
+
+        ovmf = (inputs.nixpkgs-old.legacyPackages.${system}.OVMF.override {secureBoot = true;}).fd;
+        #ovmf = (inputs.nixpkgs.legacyPackages.${system}.OVMF.override {secureBoot = true;}).fd;
       };
       devShells.default = pkgs.mkShell {
         buildInputs = with pkgs; [ uefi-run sbsigntool ];
-        ovmf = (pkgs.OVMF.override {secureBoot = true;}).fd;
+        ovmf = self'.packages.ovmf;
       };
     };
   });
